bf399b20d662526ac5907de9d7f2f7037f6c801c
jupitters <sampaiobale@gmail.com>
2026-03-13 10:50:04 -0300

    transcription controller

--- src/main/java/com/jupitters/audio_transcriber/TranscriptionController.java
+++ src/main/java/com/jupitters/audio_transcriber/TranscriptionController.java
@@ -1,4 +1,47 @@
 package com.jupitters.audio_transcriber;
 
+import org.springframework.ai.audio.transcription.AudioTranscription;
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/transcribe")
 public class TranscriptionController  {
+    private final OpenAiAudioTranscriptionModel transcriptionModel;
+
+    public TranscriptionController(OpenAiAudioTranscriptionModel transcriptionModel) {
+        this.transcriptionModel = transcriptionModel;
+    }
+
+    public ResponseEntity<String> transcribeAudio(@RequestParam("file") MultipartFile file) throws IOException {
+        File tempFile = File.createTempFile("audio", ".wav");
+        file.transferTo(tempFile);
+
+        OpenAiAudioTranscriptionOptions transcriptionOptions = OpenAiAudioTranscriptionOptions.builder()
+                .responseFormat(OpenAiAudioApi.TranscriptResponseFormat.TEXT)
+                .language("en")
+                .temperature(0f)
+                .build();
+
+        FileSystemResource audioFile = new FileSystemResource(tempFile);
+
+        AudioTranscriptionPrompt transcriptionRequest = new AudioTranscriptionPrompt(audioFile);
+        AudioTranscriptionResponse response = transcriptionModel.call(transcriptionRequest);
+
+
+    }
 }
